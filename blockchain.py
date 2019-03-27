2c042674ca763b4f94669e93c295e18208fb1298
Thiago Krempser <tkrempser@gmail.com>
2019-03-27 15:22:55 -0300

    Add an options menu

--- blockchain.py
+++ blockchain.py
@@ -12,8 +12,8 @@ def add_transaction(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_input():
-    """Get the user input and returns it as float.
+def get_transaction_amount():
+    """Get the user trasaction amount input and returns it as float.
 
     Returns:
         float: The user amount.
@@ -21,12 +21,34 @@ def get_input():
     return float(input('Type the amount please: '))
 
 
-while True:
-    # Get the first transaction
-    add_transaction(get_input())
-
-    # Output the blockchain
+def print_blockchain_blocks():
+    """Output the blockchain blocks"""
     for block in blockchain:
-        print(block)
+        print(block)   
+
+
+def show_options():
+    print('Select an option')
+    print('1: Add a new transaction')
+    print('2: Output blockchain')
+    print('q: Quit')
+
+
+def get_choice():
+    return input('Your choice: ')
+
+
+while True:
+    show_options()
+    choice = get_choice()
+
+    if choice == '1':
+        add_transaction(get_transaction_amount())
+    elif choice == '2':
+        print_blockchain_blocks()
+    elif choice == 'q':
+        break
+    else:
+        print('Invalid option.')
 
 print('Done!')
