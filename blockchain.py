1da1b18477641ee11cf00c46928f4ac9dfed5d44
Thiago Krempser <tkrempser@gmail.com>
2019-03-30 17:06:36 -0300

    Add function to get last block

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,17 @@
 blockchain = []
 
 
+def get_last_block():
+    """Returns the last blockchain value.
+    
+    Returns:
+        The last blockchain value or None if the blockchain is empty.
+    """
+    if blockchain:
+        return blockchain[-1]
+    return None
+
+
 def add_transaction(transaction_amount, last_transaction=[1]):
     """Append a new transaction to the blockchain.
     
@@ -9,6 +20,9 @@ def add_transaction(transaction_amount, last_transaction=[1]):
         transaction_amount (int): The amount value to be appended.
         last_transaction (int): The last transaction on the blockchain (default [1]).
     """
+    if not last_transaction:
+        last_transaction = [1]
+
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -23,11 +37,15 @@ def get_transaction_amount():
 
 def print_blockchain_blocks():
     """Output the blockchain blocks"""
-    for block in blockchain:
-        print(block)   
+    print('-' * 30)
+    print('Current blockchain: ' + str(blockchain))
+    for index, block in enumerate(blockchain):
+        print('Block ' + str(index) + ': ' + str(block))
 
 
 def show_options():
+    """Show available options"""
+    print('-' * 30)
     print('Select an option')
     print('1: Add a new transaction')
     print('2: Output blockchain')
@@ -43,7 +61,7 @@ while True:
     choice = get_choice()
 
     if choice == '1':
-        add_transaction(get_transaction_amount())
+        add_transaction(get_transaction_amount(), get_last_block())
     elif choice == '2':
         print_blockchain_blocks()
     elif choice == 'q':
